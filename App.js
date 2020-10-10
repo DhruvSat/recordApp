ba18d03cceac5204982cdfafa32249b767b992e2
Dwarkesh Kaswala <dwarcash@gmail.com>
2020-10-10 18:31:10 +0530

    hhh

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
 import 'react-native-gesture-handler';
 // import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
@@ -29,28 +29,28 @@ const Stack = createStackNavigator();
 //         <Stack.Screen name="Register" component={RegisterScreen} />
 //       </Stack.Navigator>
 //     </NavigationContainer>
-    
+
 //   );
 // }
 
-function App(){
+function App() {
 
-   
-  
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen 
+        <Stack.Screen
           name='LoginScreen'
           component={LoginScreen}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen 
+        <Stack.Screen
           name='RegisterScreen'
           component={RegisterScreen}
         />
       </Stack.Navigator>
-     </NavigationContainer>
+    </NavigationContainer>
   );
 };
 
