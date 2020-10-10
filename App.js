8b374868866804bb45e6bee36877ac809452ca2f
DhruvSat <43302075+DhruvSat@users.noreply.github.com>
2020-10-10 17:14:09 +0530

    login screen and register screen route

--- App.js
+++ App.js
@@ -9,28 +9,58 @@ import {
   StatusBar,
 } from 'react-native';
 
-import LoginScreen from './src/screens/loginScreen'
+import 'react-native-gesture-handler';
+// import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer} from '@react-navigation/native'
+
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 
-const HomeScreen = () => {
+const Stack = createStackNavigator();
 
+// function App() {
+//   return (
+
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+//         <Stack.Screen name="Register" component={RegisterScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+    
+//   );
+// }
+
+function App(){
+
+   
+  
   return (
-    <View style={styles.container}>
-      <StatusBar
-        backgroundColor= "#000000"
-        basstyle= "light-content"
-      />
-      <LoginScreen/>
-    </View>
+   
+    
+    <NavigationContainer>
+     
+
+       
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen 
+          name='LoginScreen'
+          component={LoginScreen}
+          options={{headerShown: false}}
+                  
+
+        />
+        <Stack.Screen 
+          name='RegisterScreen'
+          component={RegisterScreen}
+        />
+      </Stack.Navigator>
+     </NavigationContainer>
+    
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:'#000000',
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent:'center'
-  }
-})
-export default HomeScreen;
+
+export default App;
