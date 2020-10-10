c26ec1e1ff5b9576593f870d9bfa40ee908a1424
Dwarkesh Kaswala <dwarcash@gmail.com>
2020-10-10 17:47:40 +0530

    backend done

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,50 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { firebase } from '../../firebase/config'
 import {
-    Text, 
-    StyleSheet, 
-    View, 
-    Image, 
-    TextInput, 
+    Text,
+    StyleSheet,
+    View,
+    Image,
+    TextInput,
     TouchableOpacity,
-    ScrollView, 
-    KeyboardAvoidingView } from 'react-native';
+    ScrollView,
+    KeyboardAvoidingView
+} from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 
-const LoginScreen = ()=>{
-    return(
+const LoginScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onLoginPress = () => {
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                const email = response.user.email
+                const usersRef = firebase.firestore().collection('users')
+                usersRef.doc(email).get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            alert("User does not exist anymore.")
+                            return;
+                        }
+                        const user = firestoreDocument.data()
+                        navigation.navigate('Home', { user })
+                    })
+                    .catch(error => {
+                        alert(error)
+                    });
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }
+
+
+
+    return (
         <View style={styles.container}>
             <ScrollView keyboardShouldPersistTaps="handled">
-            <KeyboardAvoidingView enabled> 
-                <Image style={{width: 250, height: 200, resizeMode: 'contain', margin: 30, marginTop:90, marginBottom:40 }}
-                source={require('../../assets/logo.png')}
-                />
+                <KeyboardAvoidingView enabled>
 
-                <TextInput style={styles.inputBox} 
-                underlinecolorAndroid='rgba(0,0,0,0)' 
-                placeholder='Email'
-                placeholderTextColor='#ffffff'
-                />
+                    <Image style={{ width: 250, height: 200, resizeMode: 'contain', margin: 30, marginTop: 90, marginBottom: 40 }}
+                        source={require('../../assets/logo.png')}
+                    />
 
-                <TextInput style={styles.inputBox} 
-                underlinecolorAndroid='rgba(0,0,0,0)' 
-                placeholder='Password'
-                secureTextEntry={true}
-                placeholderTextColor='#ffffff'
-                />
-                <TouchableOpacity >
-                <Text style={styles.forgotButton}>Forgot Password ?</Text>
-                </TouchableOpacity>
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Email'
+                        placeholderTextColor='#ffffff'
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
+                    />
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}> Login </Text>
-                </TouchableOpacity>
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Password'
+                        secureTextEntry={true}
+                        placeholderTextColor='#ffffff'
+                        onChangeText={(text) => setPassword(text)}
+                        value={password}
+                    />
+                    <TouchableOpacity >
+                        <Text style={styles.forgotButton}>Forgot Password ?</Text>
+                    </TouchableOpacity>
 
                 <View style={styles.registerHere}>
                     <Text style={styles.inputText}>Don't have an account yet? </Text>
                     <TouchableOpacity><Text style={styles.registerButton}> Register </Text></TouchableOpacity>                                                                                   
                 </View>
                 {/* <View style={{flexDirection:'row'}}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => onLoginPress()}
+                    >
+                        <Text style={styles.buttonText}> Login </Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.registerHere}>
+                        <Text style={styles.inputText}>Don't have an account yet? </Text>
+                        <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}><Text style={styles.registerButton}> Register </Text></TouchableOpacity>
+                    </View>
+                    {/* <View style={{flexDirection:'row'}}>
                 <TouchableOpacity>
                      <Text style={styles.socialButton1}>
                      <FontAwesome5 name={'facebook'} size={40} color="#3b5998"/>
@@ -56,7 +101,7 @@ const LoginScreen = ()=>{
                      </Text>
                 </TouchableOpacity>
                 </View> */}
-            </KeyboardAvoidingView>
+                </KeyboardAvoidingView>
             </ScrollView>
         </View>
     );
@@ -66,7 +111,7 @@ const styles = StyleSheet.create({
     container: {
         flexGrow:1,
     },
-    
+
     inputBox: {
         width:300,
         backgroundColor:'rgba(255,255,255,0.3)',
@@ -76,20 +121,20 @@ const styles = StyleSheet.create({
         color: '#ffffff',
         marginVertical: 10
     },
-    
+
     buttonText: {
-        fontSize:15,
-        fontWeight:'500',
-        color:'#ffffff',
-        textAlign:'center'
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#ffffff',
+        textAlign: 'center'
     },
 
     button: {
-        backgroundColor:'rgba(255,255,255,0.3)',
+        backgroundColor: 'rgba(255,255,255,0.3)',
         borderRadius: 40,
         color: '#ffffff',
         marginVertical: 15,
-        textAlign:'center',
+        textAlign: 'center',
         paddingVertical: 9,
         height: 45,
         marginLeft: 100,
@@ -101,28 +146,28 @@ const styles = StyleSheet.create({
     registerHere: {
         flexGrow: 1,
         alignItems: 'flex-end',
-        justifyContent:'center',
+        justifyContent: 'center',
         paddingVertical: 10,
-        flexDirection:'row',
-        marginTop:150,
+        flexDirection: 'row',
+        marginTop: 150,
     },
 
     inputText: {
-        fontSize:16,
-        color:'#ffffff',
+        fontSize: 16,
+        color: '#ffffff',
     },
 
     registerButton: {
-        color:'#ffffff',
+        color: '#ffffff',
         fontSize: 16,
         fontWeight:'bold',
         
     },
     forgotButton: {
-        fontSize:14,
-        color:'#ffffff',
-        paddingHorizontal:10,
-        fontStyle:'italic'
+        fontSize: 14,
+        color: '#ffffff',
+        paddingHorizontal: 10,
+        fontStyle: 'italic'
     },
 
     //   socialButton1:{
--- src/screens/registerScreen.js
+++ src/screens/registerScreen.js
@@ -1,22 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { firebase } from '../../firebase/config'
 import {
-    Text, 
-    StyleSheet, 
-    View, 
-    Image, 
-    TextInput, 
+    Text,
+    StyleSheet,
+    View,
+    TextInput,
     TouchableOpacity,
-    ScrollView, 
-    KeyboardAvoidingView } from 'react-native';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+    ScrollView,
+    KeyboardAvoidingView
+} from 'react-native';
 
 
-const RegisterScreen = ()=>{
-    return
+const RegisterScreen = ({ navigation }) => {
+
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const onRegisterPress = () => {
+        if (password != confirmPassword) {
+            alert("Password does not match")
+            return
+        }
+
+        //creating user
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                const uid = response.user.uid
+                const data = {
+                    id: uid,
+                    email,
+                    fullName
+                };
+
+                const usersRefs = firebase.firestore().collection('users')
+                usersRefs.doc(email).set(data)
+                    .then(() => {
+                        navigation.navigate('Home', { user: data })
+                    })
+                    .catch((error) => {
+                        alert(error)
+                    });
+            })
+            .catch((error) => {
+                alert(error)
+            });
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView keyboardShouldPersistTaps="handled">
+                <KeyboardAvoidingView enabled>
+
+                    <Text style={styles.signupText}>SignUp!</Text>
+
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Name'
+                        placeholderTextColor='#ffffff'
+                        onChangeText={(text) => setFullName(text)}
+                        value={fullName}
+                    />
+
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Email'
+                        placeholderTextColor='#ffffff'
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
+                    />
+
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Mobile No.'
+                        placeholderTextColor='#ffffff'
+                    />
+
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Password'
+                        secureTextEntry={true}
+                        onChangeText={(text) => setPassword(text)}
+                        value={password}
+                        placeholderTextColor='#ffffff'
+                    />
+
+                    <TextInput style={styles.inputBox}
+                        underlinecolorAndroid='rgba(0,0,0,0)'
+                        placeholder='Confirm Password'
+                        secureTextEntry={true}
+                        onChangeText={(text) => setConfirmPassword(text)}
+                        value={confirmPassword}
+                        placeholderTextColor='#ffffff'
+                    />
+
+
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => onRegisterPress()}>
+                        <Text style={styles.buttonText}> Register Account </Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.registerHere}>
+                        <Text style={styles.inputText}>Already have an Account? </Text>
+                        <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}><Text style={styles.registerButton}> Login </Text></TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-    
+
+    container: {
+        flexGrow: 1,
+    },
+
+    signupText: {
+        fontSize: 30,
+        color: '#ffffff',
+    },
+
+    buttonText: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#ffffff',
+        textAlign: 'center'
+    },
+
+    button: {
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        borderRadius: 40,
+        color: '#ffffff',
+        marginVertical: 15,
+        textAlign: 'center',
+        paddingVertical: 9,
+        height: 45,
+        marginLeft: 100,
+        marginRight: 100,
+        marginTop: 20,
+        marginBottom: 20
+    },
+
+    inputBox: {
+        width: 300,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        borderRadius: 25,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#ffffff',
+        marginVertical: 10
+    },
+
+    registerHere: {
+        flexGrow: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        flexDirection: 'row',
+        marginTop: 150,
+    },
+
+    inputText: {
+        fontSize: 16,
+        color: '#ffffff',
+    },
+
+    registerButton: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
 })
 
 export default RegisterScreen;
