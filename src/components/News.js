9ef795767dc7ff1265eb14c9cd4f6b11418a702f
Dwarkesh Kaswala <dwarcash@gmail.com>
2020-10-12 16:07:23 +0530

    Carousel implimented

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import storage from '@react-native-firebase/storage';
+import { FlatListSlider } from 'react-native-flatlist-slider';
 import { View, Text, Image } from 'react-native'
 
 
@@ -7,7 +8,9 @@ function listFilesAndDirectories(reference, pageToken) {
     return reference.list({ pageToken }).then(result => {
         // Loop over each item
         result.items.forEach(ref => {
-            console.log(ref.fullPath);
+            getUrl(ref.fullPath)
+            // console.log(ref.fullPath);
+
         });
 
         if (result.nextPageToken) {
@@ -24,31 +27,49 @@ listFilesAndDirectories(reference).then(() => {
     console.log('Finished listing');
 });
 
-async function getUrl() {
+async function getUrl(path) {
 
     url = await storage()
-        .ref('news/banner_01.jpg')
+        .ref(path)
         .getDownloadURL();
     console.log(`URL = ${url}`)
-
-
 }
 
 
 const News = () => {
 
-    const url = 'https://firebasestorage.googleapis.com/v0/b/crime-record-app.appspot.com/o/news%2Fbanner_01.jpg?alt=media&token=a3cca5e7-fb49-492a-88df-cd5c5c2ec8c1'
-    //getUrl();
+    const images = [
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/crime-record-app.appspot.com/o/news%2Fbanner_01.jpg?alt=media&token=a3cca5e7-fb49-492a-88df-cd5c5c2ec8c1',
+            
+        },
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/crime-record-app.appspot.com/o/news%2Fbanner_04.jpg?alt=media&token=18d6a477-6fc8-4768-a3c0-73847a3b1bdb',
+        },
+    ]
+
+
     return (
         <View>
             <Text>
                 LUCKY if you see this
-                {/* URL = {url} */}
+               
             </Text>
-            <Image
-                source={{ uri: url }}
-                style={{ width: 1600, height: 425 }}
-            />
+            <View>
+                <FlatListSlider
+                    data={images}
+                    height={120}
+                    // timer={5000}
+                    onPress={item => alert(JSON.stringify(item))}
+                    contentContainerStyle={{  }}
+                    indicatorContainerStyle={{ position: 'absolute', bottom: 10 }}
+                    indicatorActiveColor={'#8e44ad'}
+                    indicatorInActiveColor={'#ffffff'}
+                    indicatorActiveWidth={12}
+                    animation
+                />
+            </View>
+            
         </View>
     )
 }
